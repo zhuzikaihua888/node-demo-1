ff9dcdbf67020fcd16cbfc2b8742b84ff2e8f030
zhuzikaihua888 <76236219+zhuzikaihua888@users.noreply.github.com>
2021-02-08 23:38:28 +0800

    Update node-demo.js

--- node-demo.js
+++ node-demo.js
@@ -24,10 +24,10 @@ var server = http.createServer(function(request, response){
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`
-    <link rel="stylesheet" href="./style.css">
-    <h1>你好</h1>
-  `)
+   response.write(`
+      <link rel="stylesheet" href="./style.css">
+      <h1>你好</h1>
+    `)
     response.end()
   } else if(path === './style.css'){
     response.statusCode = 200
