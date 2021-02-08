3e7ab4f44c9432c7e7b2348c36e3973dd7b1c421
zhuzikaihua888 <76236219+zhuzikaihua888@users.noreply.github.com>
2021-02-08 23:28:38 +0800

    Update node-demo.js

--- node-demo.js
+++ node-demo.js
@@ -24,9 +24,12 @@ var server = http.createServer(function(request, response){
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`二哈`)
+    response.write(`
+    <link rel="stylesheet" href="./style.css">
+    <h1>你好</h1>
+  `)
     response.end()
-  } else if(path === '/x'){
+  } else if(path === './style.css'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
     response.write(`body{color: red;}`)
